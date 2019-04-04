a8223932a55c608371093883ec2e7e9454b932a9
yan <905268497@qq.com>
2019-04-04 14:14:00 +0800

    dd

---
+++ Implements/src/main/java/com/watermelon/implementstest/controller/Test.java
@@ -0,0 +1,14 @@
+package com.watermelon.implementstest.controller;
+
+import org.springframework.boot.jackson.JsonComponent;
+
+/**
+ * Demo class
+ *
+ * @author yan
+ * @date 2019/04/04
+ */
+@JsonComponent
+public class Test {
+
+}
